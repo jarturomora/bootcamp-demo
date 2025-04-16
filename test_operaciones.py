e149fd6ab620421737049119d5880b2d48d5ab81
jarturomora <jarturomora@gmail.com>
2025-04-16 17:44:03 +0200

    Provoca error en pruebas de suma

--- test_operaciones.py
+++ test_operaciones.py
@@ -7,7 +7,7 @@ class TestOperaciones(unittest.TestCase):
 
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
-        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, 1), 1)
         self.assertEqual(sumar(-1, -1), -2)
 
     def test_restar(self):
